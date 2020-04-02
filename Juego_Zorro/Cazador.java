87bcb870714685160c7b7817c70d75a9ad1f5b6a
Aczayacatl BS <52363947+Aczayacatl@users.noreply.github.com>
2020-04-02 08:17:39 -0600

    Add files via upload

---
+++ Juego_Zorro/Cazador.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Cazador here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Cazador extends Enemigo
+{
+    /**
+     * Act - do whatever the Cazador wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        getImage();
+    }    
+}
---
+++ Juego_Zorro/Fondo.java
@@ -0,0 +1,10 @@
+/**
+ * Write a description of class Escenario here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public enum Fondo  
+{
+    JUEGO, MENU, BIENVENIDA1, BIENVENIDA2
+}
